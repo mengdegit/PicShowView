c65807ce57f02d2b3472ad4d823e023287979d0d
mengdegit <meng644542847@163.com>
2017-06-27 09:09:20 +0800

    first commit

---
+++ pictureshowview/src/main/java/com/joker/pictureshowview/PicShowView.java
@@ -0,0 +1,14 @@
+package com.joker.pictureshowview;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+/**
+ * Created by aa on 2017/6/26.
+ */
+
+public class PicShowView extends ImageView{
+    public PicShowView(Context context) {
+        super(context);
+    }
+}
