a7af7092ac2afffc9df79f73717cd76c956d1751
mengdegit <meng644542847@163.com>
2017-07-06 10:57:07 +0800

    测试github提交

--- app/src/main/java/com/joker/picshowview/MainActivity.java
+++ app/src/main/java/com/joker/picshowview/MainActivity.java
@@ -2,6 +2,7 @@ package com.joker.picshowview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.test_github);
+        tv.setText("测试Github提交");
     }
 }
